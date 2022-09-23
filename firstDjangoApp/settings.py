debc61a06f61a7859ccc298df974889c36849615
TecHeReTiC <talex73@mail.ru>
2022-09-23 23:07:39 +0500

    Main page has been added. Skills are in progress.

--- firstDjangoApp/settings.py
+++ firstDjangoApp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -110,7 +109,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- firstDjangoApp/urls.py
+++ firstDjangoApp/urls.py
@@ -13,11 +13,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+all_projects = [
+    [
+        {'title': 'Platformetic', 'image': '/images/platformetic.png',
+         'descr': 'Simple platformer with elements of bullet hell',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/PygamePlatformer',
+                   'img': '/images/github.png'}]},
+        {'title': 'Dungetic', 'image': '/images/dungetic.png',
+         'descr': 'Rogue-like game with elements of RPG',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/Dungetic',
+                   'img': '/images/github.png'}]},
+        {'title': 'Platformetic', 'image': '/images/platformetic.png',
+         'descr': 'Simple platformer with elements of bullet hell',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/PygamePlatformer',
+                   'img': '/images/github.png'}]}
+    ],
+
+    [
+        {'title': 'Platformetic', 'image': '/images/platformetic.png',
+         'descr': 'Simple platformer with elements of bullet hell',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/PygamePlatformer',
+                   'img': '/images/github.png'}]},
+        {'title': 'Platformetic', 'image': '/images/platformetic.png',
+         'descr': 'Simple platformer with elements of bullet hell',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/PygamePlatformer',
+                   'img': '/images/github.png'}]},
+        {'title': 'Platformetic', 'image': '/images/platformetic.png',
+         'descr': 'Simple platformer with elements of bullet hell',
+         'refs': [{'ref': 'https://github.com/TecHeReTiC3141/PygamePlatformer',
+                   'img': '/images/github.png'}]}
+    ],
+
+]
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("polls/", include('polls.urls')),
-
+    path("", TemplateView.as_view(template_name='main_page.html',
+                                  extra_context={'projects': all_projects}
+                                  ))
 ]
